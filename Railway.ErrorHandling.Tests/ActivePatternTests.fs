c981286126f57fcb5ce4f9bde25861cc6a75973e
Yangs Richie <richieyangs@gmail.com>
2019-11-09 00:04:35 +0800

    active pattern

---
+++ Railway.ErrorHandling.Tests/ActivePatternTests.fs
@@ -0,0 +1,29 @@
+﻿namespace Railway.ErrorHandling.Tests.ActivePatternTests
+open System
+open Xunit
+
+[<AutoOpen>]
+module ActivePattern =
+    let (|Int|_|) (str: string) =
+        match Int32.TryParse(str) with
+        | (true, value) -> Some(value)
+        | _ -> None
+        
+    let (|Bool|_|) (str: string) =
+        match Boolean.TryParse(str) with
+        | (true, v) -> Some(true)
+        | _ -> None
+        
+    let testParse str =
+        match str with
+        | Int value -> sprintf "int value %i" value
+        | Bool value -> sprintf "bool value %b" value
+        | _ -> sprintf "unkown string %A" str
+
+module Tests =
+    
+    [<Fact>]
+    let ``Test parse int string should get int value``() =
+        let value = testParse "1"
+
+        Assert.Equal(value, "int value 1")
---
+++ Railway.ErrorHandling/ErrorHandling.fs
@@ -0,0 +1,70 @@
+﻿namespace Railway.ErrorHandling
+open System
+open System.Collections.Generic
+
+type Result<'TSuccess, 'TMessage> =
+    | OK of 'TSuccess * 'TMessage list
+    | Bad of 'TMessage list
+    
+    static member FailWith (messages : 'TMessage seq) = Bad(messages |> Seq.toList)
+    
+    static member FailWith (message : 'TMessage) = Bad([message])
+    
+    static member Succeed (value : 'TSuccess) = Ok(value, [])
+    
+    static member Succeed (value : 'TSuccess, message : 'TMessage) = Ok(value, [message])
+    
+    static member Succeed (value : 'TSuccess, messages : 'TMessage seq) = Ok(value, messages |> Seq.toList)
+    
+//     static member Try(func: Func<'TSuccess>) : Result<'TSuccess,exn> =        
+//        try
+//            Ok(func.Invoke(),[])
+//        with
+//        | exn -> Bad[exn]
+//
+    override x.ToString() =
+        match x with
+        | OK(v, messages) -> sprintf "%A - %s" v (String.Join(Environment.NewLine, messages |> List.map (fun m -> m.ToString())))
+        | Bad(messages) -> sprintf "%s" (String.Join(Environment.NewLine, messages |> List.map (fun m -> m.ToString())))
+        
+    
+module Trail =
+    
+    let inline ok<'TSuccess, 'TMessage> (x : 'TSuccess) = OK(x, [])
+    
+    let inline pass<'TSuccess, 'TMessage> (x : 'TSuccess) = OK(x, [])
+    
+    let inline warn<'TSuccess, 'TMessage> (x : 'TSuccess) (message : 'TMessage) = Ok(x, [message])
+    
+    let inline fail<'TMessage> (message : 'TMessage) = Bad([message])
+    
+    let inline failed result = 
+        match result with
+        | Bad _ -> true
+        | _ -> false
+    
+    // let inline either fSuccess fFailure result =
+    //     match result with
+    //     | Ok(value, messages) -> fSuccess (value, messages)
+    //     | Bad(messages) -> fFailure messages
+    
+    // let inline returnOrFail result =
+    //     let inline raiseExn msgs =
+    //         msgs
+    //         |> Seq.map (sprintf "%O")
+    //         |> String.concat (Environment.NewLine + "\t")
+    //         |> failwith
+    //     either fst raiseExn result
+    
+    // let inline mergeMessages messages2 result=
+    //     let inline fSuccess (value, messages) = Ok(value, messages @ messages2)
+    //     let inline fFailure (messages) = Bad(messages @ messages2)
+    //     either fSuccess fFailure result
+        
+    // let inline bind f result =
+    //     let inline fSuccess (value, messages) = f value |> mergeMessages messages
+    //     let inline fFailure messages = Bad(messages)
+        
+    //     either fSuccess fFailure result
+        
+    
